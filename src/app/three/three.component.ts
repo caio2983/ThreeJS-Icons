bfc3f80323f1679d22b6f7b108f135d1c501b737
caio2983 <caioluna2002@gmail.com>
2025-04-11 16:54:59 -0300

    add home icon

--- src/app/three/three.component.ts
+++ src/app/three/three.component.ts
@@ -1,5 +1,6 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import * as THREE from 'three';
+import { SVGLoader } from 'three/examples/jsm/Addons.js';
 
 @Component({
   selector: 'app-three',
@@ -10,23 +11,23 @@ export class ThreeComponent implements AfterViewInit {
   @ViewChild('canvas', { static: true })
   canvasRef!: ElementRef<HTMLCanvasElement>;
 
+  // private scene!: THREE.Scene;
   private scene!: THREE.Scene;
   private camera!: THREE.PerspectiveCamera;
   private renderer!: THREE.WebGLRenderer;
-  private cube!: THREE.Mesh;
   private animationId!: number;
+  group!: THREE.Group;
+
+  svg: string = 'home_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg';
 
   ngAfterViewInit(): void {
     this.initThree();
     this.animate();
   }
-
   private initThree(): void {
     const canvas = this.canvasRef.nativeElement;
-
-    this.scene = new THREE.Scene();
-
     const aspectRatio = window.innerWidth / window.innerHeight;
+
     this.camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
     this.camera.position.z = 5;
 
@@ -35,27 +36,56 @@ export class ThreeComponent implements AfterViewInit {
       antialias: true,
     });
     this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.renderer.setClearColor(new THREE.Color('#e0e0e0'), 1);
+    this.renderer.setClearColor(new THREE.Color('black'), 1);
 
-    const light = new THREE.PointLight(0xffffff, 1);
-    light.position.set(5, 5, 5);
-    this.scene.add(light);
+    this.scene = new THREE.Scene();
 
-    const ambientLight = new THREE.AmbientLight(0x404040); // luz ambiente fraca
-    this.scene.add(ambientLight);
+    const loader = new SVGLoader();
+    loader.load(
+      this.svg,
+      (data) => {
+        const paths = data.paths;
+        const group = new THREE.Group();
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshStandardMaterial({ color: 0x0077ff });
-    this.cube = new THREE.Mesh(geometry, material);
+        for (let i = 0; i < paths.length; i++) {
+          const path = paths[i];
+          console.log(path);
 
-    this.scene.add(this.cube);
+          const material = new THREE.MeshBasicMaterial({
+            color: 'white',
+            side: THREE.DoubleSide,
+            depthWrite: false,
+          });
+
+          const shapes = SVGLoader.createShapes(path);
+          for (let j = 0; j < shapes.length; j++) {
+            const shape = shapes[j];
+            const geometry = new THREE.ShapeGeometry(shape);
+            const mesh = new THREE.Mesh(geometry, material);
+            group.add(mesh);
+          }
+        }
+
+        group.scale.set(0.0005, 0.0005, 0.0005);
+        group.rotation.x = Math.PI;
+        group.position.set(0, 0, 0);
+        this.scene.add(group);
+
+        this.group = group;
+        console.log(group);
+      },
+      (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+      },
+      (error) => {
+        console.error('Erro ao carregar SVG:', error);
+      }
+    );
   }
 
   private animate = () => {
     this.animationId = requestAnimationFrame(this.animate);
-
-    this.cube.rotation.x += 0.01;
-    this.cube.rotation.y += 0.01;
+    this.group.rotation.y += 0.015;
 
     this.renderer.render(this.scene, this.camera);
   };
